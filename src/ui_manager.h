306c11f1fd7544fd9ff059da9e2e4c055e77e911
Guido Günther <agx@sigxcpu.org>
2020-09-11 18:15:02 +0200

    treewide: Use new style function definitions

--- src/ui_manager.h
+++ src/ui_manager.h
@@ -7,7 +7,7 @@
 
 struct ui_manager;
 
-struct ui_manager *squeek_uiman_new();
+struct ui_manager *squeek_uiman_new(void);
 void squeek_uiman_set_output(struct ui_manager *uiman, struct squeek_output_handle output);
 uint32_t squeek_uiman_get_perceptual_height(struct ui_manager *uiman);
 
