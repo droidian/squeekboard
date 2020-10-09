6871452c7b840ca16e0aef80dafdea372abe041a
Arnaud Ferraris <arnaud.ferraris@gmail.com>
2020-10-10 01:40:12 +0200

    keyboards: add wide terminal layout
    This is a copy of the `terminal` layout with modified key dimensions to 
    fit a wide arrangement.

--- src/resources.rs
+++ src/resources.rs
@@ -38,6 +38,7 @@ const KEYBOARDS: &[(*const str, *const str)] = &[
     ("ua", include_str!("../data/keyboards/ua.yaml")),
     // layout+overlay
     ("terminal", include_str!("../data/keyboards/terminal.yaml")),
+    ("terminal_wide", include_str!("../data/keyboards/terminal_wide.yaml")),
     // Overlays
     ("emoji", include_str!("../data/keyboards/emoji.yaml")),
 ];
