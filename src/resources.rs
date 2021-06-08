6409ff374848d0cd2cb105028867669004929911
Patrick Jörg <mobian@mobian.fritz.box>
2021-06-08 17:06:36 +0000

    Introducing ch+de layout and modified ch.yaml fallback
    Autor:           Patrick Jörg <patrickjoerg@gmx.ch>

--- src/resources.rs
+++ src/resources.rs
@@ -29,6 +29,7 @@ static KEYBOARDS: &[(&'static str, &'static str)] = &[
     ("br", include_str!("../data/keyboards/br.yaml")),
     
     ("ch+fr", include_str!("../data/keyboards/ch+fr.yaml")),
+    ("ch+de", include_str!("../data/keyboards/ch+de.yaml")),
     ("ch", include_str!("../data/keyboards/ch.yaml")),
 
     ("de", include_str!("../data/keyboards/de.yaml")),
